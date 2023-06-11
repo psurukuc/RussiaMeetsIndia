0aba24951bc4b1811a8e21c899b5ca0f91be8b33
psurkuc <psurukuc@hawk.iit.edu>
2023-06-11 15:34:36 -0400

    Mods for running webserver

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var gulp = require('gulp');
+var webserver = require('gulp-webserver');
 var sass = require('gulp-sass')(require('sass'));
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
@@ -26,5 +27,26 @@ gulp.task('minify-js', function () {
         .pipe(gulp.dest('./js'));
 });
 
+gulp.task('webserver', function() {
+    gulp.src('/Users/psurukuc/Applications/RussiaMeetsIndia/')
+      .pipe(webserver({
+        livereload: true,
+        directoryListing: true,
+        open: true,
+        fallback: 'index.html'
+        // port: 6000	// set a port to avoid conflicts with other local apps
+      }));
+});
+
+// gulp.task('server', function() {
+//     gulp.src('/Users/psurukuc/Applications/RussiaMeetsIndia/')	
+//       .pipe(server({
+//         livereload: true,
+//         open: true,
+//         port: 6000	// set a port to avoid conflicts with other local apps
+//       }));
+//   });
+
+
 // default task
-gulp.task('default', gulp.series('sass', 'minify-js'));
+gulp.task('default', gulp.series('sass', 'minify-js', 'webserver'));
